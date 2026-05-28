fde52ca994e39f9cb063beadb651ced7729555d2
Joso1207 <joso1207@student.miun.se>
2026-05-28 13:45:43 +0200

    Init

---
+++ src/main/java/org/chasapi/activityreccomender/ActivityReccomenderApplication.java
@@ -0,0 +1,13 @@
+package org.chasapi.activityreccomender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityReccomenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityReccomenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chasapi/activityreccomender/ActivityReccomenderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chasapi.activityreccomender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityReccomenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
