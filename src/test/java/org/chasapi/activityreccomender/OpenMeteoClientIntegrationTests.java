844ff9ee7acbfc4405e139132a353cc26edf94f5
Joso1207 <joso1207@student.miun.se>
2026-06-04 18:21:45 +0200

    Exception handling and verifying behavior of ActivityService.java

--- src/test/java/org/chasapi/activityreccomender/OpenMeteoClientIntegrationTests.java
+++ src/test/java/org/chasapi/activityreccomender/OpenMeteoClientIntegrationTests.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-class OpenMeteoClientIT {
+class OpenMeteoClientIntegrationTests {
 
 
     @Autowired
